ece2a17f1c73e22acf20205e7884ecfd45e82e31
DavidS17GitHub <108815743+DavidS17GitHub@users.noreply.github.com>
2024-03-14 12:31:17 -0500

    Section 04. API Setup

--- 04. API Basics/DotnetAPI/Program.cs
+++ 04. API Basics/DotnetAPI/Program.cs
@@ -7,26 +7,44 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// We need to allow CORS so other aplications comming from different origins
+// than the ones set in the launchSettings.json can access our application, like for example
+// frontend clients
+builder.Services.AddCors((options) => 
+    {
+        options.AddPolicy("DevCors", (corsBuilder) =>
+            {
+                corsBuilder.WithOrigins("http://localhost:4200, http://localhost:3000, http://localhost:8000")
+                    /* 4200 (Angular),  3000 (React), 8000 (Vue) */
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+        options.AddPolicy("ProdCors", (corsBuilder) =>
+            {
+                corsBuilder.WithOrigins("https://myProductionSite.com")
+                    /* The domains that will be accessing our API */
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors"); /* Here we make use of the CORS policies we created earlier */
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 else {
-    app.UseHttpsRedirection(); // Changed so it's not the default option for our Web APIs
+    app.UseCors("ProdCors"); /* Here we make use of the CORS policies we created earlier */
+    app.UseHttpsRedirection();
 }
 
 app.MapControllers();
-
-// app.MapGet("/weatherforecast", () =>
-// {
-    
-// })
-// .WithName("GetWeatherForecast")
-// .WithOpenApi();
 
 
 
